7683ff5455ee50e9f8cbb604e625054f8f2c1b91
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-11 10:31:43 +0300

    Added importing to components in App.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,7 @@
+import Header from './components/Header.js';
+import Content from './components/Header.js';
+import Total from './components/Header.js';
+
 function App() {
   const course = "Half Stack application development";
   const parts = [ "Fundamentals of React", "Using props to pass data", "State of a component" ];
