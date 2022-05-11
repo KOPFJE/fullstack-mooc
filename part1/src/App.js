269448db6405a9f0e0e49356be1d6457c95316ac
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-11 10:20:09 +0300

    Modified app in accordance to exercise 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const part2 = "Using props to pass data"
+  const part3 = "State of a component"
+  const exercises = [ 10, 7, 14 ]
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises[0]}
+      </p>
+      <p>
+        {part2} {exercises[1]}
+      </p>
+      <p>
+        {part3} {exercises[2]}
+      </p>
+      <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
     </div>
   );
 }
