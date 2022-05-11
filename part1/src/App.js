f6a0d834edbe9350d7f66fc7d5d9c80b81d10d99
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-11 10:29:24 +0300

    Modified app.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,16 @@
 function App() {
-  const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const part2 = "Using props to pass data"
-  const part3 = "State of a component"
-  const exercises = [ 10, 7, 14 ]
+  const course = "Half Stack application development";
+  const parts = [ "Fundamentals of React", "Using props to pass data", "State of a component" ];
+  const exercises = [ 10, 7, 14 ];
 
 
 
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises[0]}
-      </p>
-      <p>
-        {part2} {exercises[1]}
-      </p>
-      <p>
-        {part3} {exercises[2]}
-      </p>
-      <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]}</p>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
     </div>
   );
 }
