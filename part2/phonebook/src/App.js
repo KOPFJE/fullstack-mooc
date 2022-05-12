d14a60c9d1362f6c729af5b7f60b27d771d1d56c
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-12 16:29:59 +0300

    Completed up to 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { 
+      name: 'Arto Hellas',
+      phoneNumber : '040-1231244'
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewPhoneNumber] = useState('');
+  
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setNewName(e.target.value);
+  }
+
+  const handlePhoneNumberChange = (e) => {
+    e.preventDefault();
+    setNewPhoneNumber(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newArr = [...persons];
+    newArr.push({ name: newName, phoneNumber : newNumber });
+    
+    if(persons.some(person => person.name.toLowerCase().trim() === newName.toLowerCase().trim())) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(newArr);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input onChange={handlePhoneNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        { persons.map(person => <li key={person.name}>{person.name} {person.phoneNumber}</li>)}
+      </ul>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
