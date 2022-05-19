63d18d407a7a2cbdf40d20edbf734d6c9c188363
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-19 10:07:30 +0300

    Added exercise 3.12.

---
+++ part4/mongodb-cli/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.beafm.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
