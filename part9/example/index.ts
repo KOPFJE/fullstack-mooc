83c6a1a79c28b3dca3d008e825f03b1290c48db4
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-05-20 15:05:54 +0300

    Exercise 9.5 completed to specifications.

--- part9/example/index.ts
+++ part9/example/index.ts
@@ -9,22 +9,23 @@ app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
 });
 app.get('/bmi', (req, res) => {
-    if(typeof req.query.height !== "string" || typeof req.query.weight !== "string") 
-        res.send( 
+    const height = req.query.height;
+    const weight = req.query.weight;
+
+    if(typeof height !== "string" || typeof weight !== "string") {
+        res.status(400).send({
+            error : "Malformatted parameters"
+        });
+    } else {
+        let bmi = calculateBmi(parseFloat(height), parseFloat(weight));
+        res.send(
             {
-                error : "Malformatted parameters!"
+                weight : weight,
+                height : height,
+                bmi : bmi
             }
         );
-    const height = parseFloat(req.query.height);
-    const weight = parseFloat(req.query.weight);
-    let bmi = calculateBmi(height, weight);
-    res.send(
-        {
-            weight : weight,
-            height : height,
-            bmi : bmi
-        }
-    );
+    };
 
 });
 const PORT = 3003;
